25b44425af34943a45f13fe0b2a13442a43e12d6
ItsODRJR <69770237+ItsODRJR@users.noreply.github.com>
2025-12-03 08:28:30 -0600

    hi

--- linked_list.cpp
+++ linked_list.cpp
@@ -5,21 +5,100 @@
 using std::cout, std::endl, std::string, std::ostream;
 
 void MyList::add(Student) {
-    // TODO(student)
+    MyNode* n = new MyNode(Student, nullptr, nullptr);
+
+    if (_head == nullptr) {
+        _head = n;
+        _tail = n;
+    } else {
+        node->previous = _tail;
+        _tail->next = n;
+        _tail = n;
+    }
+
+    _size++;
+
 }
 
 void MyList::clear() {
-    // TODO(student)
+    MyNode* curr = _head;
+
+    while (curr != nullptr) {
+        MyNode* next = curr->next;
+        delete curr;
+        curr = next;
+    }
+
+    _head = nullptr;
+    _tail = nullptr;
+    _size = 0;
 }
 
 bool MyList::remove(const Student&) {
-    // TODO(student)
+    MyNode* curr = _head;
+
+    while (curr != nullptr) {
+        if (curr->value == student) {
+            if (curr == _head) {
+                _head = curr->next;
+                if (_head) _head->previous = nullptr;
+            }
+
+            if (curr == _tail) {
+                _tail = curr->previous;
+                if (_tail) _tail->next = nullptr;
+            }
+
+            if (curr->previous) {
+                curr->previous->next = curr->next;
+            }
+            if (curr->next) {
+                curr->next->previous = curr->previous;
+            }
+
+            delete curr;
+            _size--;
+            return true;
+        }
+        curr = curr->next;
+    }
 
     return false;
 }
 
 bool MyList::insert(Student, size_t) {
-    // TODO(student)
+    if (index > _size) { 
+        return false;
+    }
 
-    return false;
+    if (index == _size) {
+        add(student);
+        return true;
+    }
+
+    if (index == 0) {
+        MyNode* node = new MyNode(student, _head, nullptr);
+        if (_head) {
+            _head->previous = node;
+        }
+        _head = node;
+        if (_tail == nullptr) {
+            _tail = node;
+        }
+        _size++;
+        return true;
+    }
+
+    MyNode* curr = _head;
+    for (size_t i = 0; i < index; i++) {
+        curr = curr->next;
+    }
+
+    MyNode* node = new MyNode(student, curr, curr->previous);
+
+    curr->previous->next = node;
+    curr->previous = node;
+
+    _size++;
+    return true;
 }
